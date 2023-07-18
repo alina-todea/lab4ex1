1956907423c4f6b152565c0c33e8c5ca39e022a4
alina-todea <137332096+alina-todea@users.noreply.github.com>
2023-07-18 23:08:38 +0300

    Add files via upload

---
+++ Dulap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace lab4ex1
+{
+    class Dulap
+    {
+
+        private int lungime;
+        private int latime;
+        private int inaltime;
+
+
+
+        public Dulap(int lungime, int latime, int inaltime)
+        {
+
+            this.lungime = lungime;
+            this.latime = latime;
+            this.inaltime = inaltime;
+        }
+
+
+        public int GetLungime()
+        {
+            return lungime;
+        }
+
+        public int GetLatime()
+        {
+            return latime;
+        }
+
+        public int GetInaltime()
+        {
+            return inaltime;
+        }
+
+        public int GetVolume(int lungime, int latime, int inaltime)
+        {
+
+            int volume = lungime * latime * inaltime;
+
+            return volume;
+        }
+
+
+
+    }
+}
